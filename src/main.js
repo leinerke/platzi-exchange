7398284cb77e4cd6bac48025c90d09e05433566b
Leinerke Suarez <leinerke2000@gmail.com>
2022-02-10 12:06:01 -0500

    Utilizar componentes de terceros.

--- src/main.js
+++ src/main.js
@@ -3,7 +3,12 @@ import App from './App.vue';
 import '@/assets/css/tailwind.css';
 import router from '@/router';
 import { dollarFilter, percetFilter } from '@/filters';
+import Chart from 'chart.js';
+import Chartkick from 'vue-chartkick';
+import { VueSpinners } from '@saeris/vue-spinners';
 
+Vue.use(VueSpinners);
+Vue.use(Chartkick.use(Chart));
 Vue.filter('dollar', dollarFilter);
 Vue.filter('percent', percetFilter);
 Vue.config.productionTip = false;
--- src/views/CoinDetail.vue
+++ src/views/CoinDetail.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="flex-col">
-    <template v-if="asset.id">
+    <div class="flex justify-center">
+      <bounce-loader :loading="isLoading" :color="'#68d391'" :size="100" />
+    </div>
+    <template v-if="!isLoading">
       <div class="flex flex-col sm:flex-row justify-around items-center">
         <div class="flex flex-col items-center">
           <img
@@ -64,6 +67,14 @@
           <span class="text-xl"></span>
         </div>
       </div>
+
+      <line-chart
+        class="my-10"
+        :colors="['orange']"
+        :min="min"
+        :max="max"
+        :data="history.map(item => [item.date, parseFloat(item.priceUsd).toFixed(2)])"
+      />
     </template>
   </div>
 </template>
@@ -75,6 +86,7 @@ export default {
   name: 'CoinDetail',
   data() {
     return {
+      isLoading: false,
       asset: {},
       history: [],
     };
@@ -95,15 +107,13 @@ export default {
   },
   methods: {
     getCoin() {
+      this.isLoading = true;
       const id = this.$route.params.id;
-      Promise.all([
-        api.getAsset(id),
-        api.getAssetHistory(id),
-      ])
-        .then(([asset, history]) => {
-          this.asset = asset;
-          this.history = history;
-        });
+      Promise.all([api.getAsset(id), api.getAssetHistory(id)]).then(([asset, history]) => {
+        this.asset = asset;
+        this.history = history;
+      })
+        .finally(() => (this.isLoading = false));
     },
   },
 };
