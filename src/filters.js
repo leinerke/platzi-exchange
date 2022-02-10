bb1ac3ae00d53253b815d1fef9bdbedbeea05086
Leinerke Suarez <leinerke2000@gmail.com>
2022-02-10 10:43:17 -0500

    Mejorar la UI con filtros.

---
+++ src/filters.js
@@ -0,0 +1,20 @@
+import numeral from 'numeral';
+
+const dollarFilter = value => {
+	if (!value) {
+		return '$ 0';
+	}
+	return numeral(value).format('($ 0.00a)');
+};
+
+const percetFilter = value => {
+	if (!value) {
+		return '0%';
+	}
+	return `${Number(value).toFixed(2)}%`;
+};
+
+export {
+	dollarFilter,
+	percetFilter,
+};
--- src/main.js
+++ src/main.js
@@ -1,11 +1,14 @@
-import Vue from 'vue'
-import App from './App.vue'
-import '@/assets/css/tailwind.css'
-import router from'@/router';
+import Vue from 'vue';
+import App from './App.vue';
+import '@/assets/css/tailwind.css';
+import router from '@/router';
+import { dollarFilter, percetFilter } from '@/filters';
 
-Vue.config.productionTip = false
+Vue.filter('dollar', dollarFilter);
+Vue.filter('percent', percetFilter);
+Vue.config.productionTip = false;
 
 new Vue({
-  router,
-  render: h => h(App),
-}).$mount('#app')
+	router,
+	render: h => h(App),
+}).$mount('#app');
