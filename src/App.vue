40fa48f296bcb8db40c0fe4fb81838732d501a6a
Leinerke Suarez <leinerke2000@gmail.com>
2022-02-10 08:45:08 -0500

    Primeros Componentes.

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,22 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <main>
+    <px-header />
+    <px-assets-table />
+  </main>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import PxHeader from '@/components/PxHeader';
+import PxAssetsTable from '@/components/PxAssetsTable';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
-}
+    PxHeader,
+    PxAssetsTable,
+  },
+};
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
